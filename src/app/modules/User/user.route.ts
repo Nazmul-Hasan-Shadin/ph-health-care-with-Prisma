7c4e96383fd2cbf881b4f010e41c71b0bec6f46e
Nazmul-Hasan-Shadin <nazmulhasanshadin000@gmail.com>
2024-11-17 00:01:49 +0600

    multer file upload system added

--- src/app/modules/User/user.route.ts
+++ src/app/modules/User/user.route.ts
@@ -1,17 +1,21 @@
 import express, { NextFunction, Request, Response } from "express";
 import { userController } from "./user.controller";
 import { jwtHelpers } from "../../../helpers/jwtHelpers";
-import config from "../../../config";
+
 import { Secret } from "jsonwebtoken";
 import { visitEachChild } from "typescript";
 import auth from "../../middlewares/auth";
 import { UserRole } from "@prisma/client";
+import multer from "multer";
+import path from "path";
+import { fileUploader } from "../../../helpers/fileUploader";
 
 const router = express.Router();
 
 router.post(
   "/",
-  auth(UserRole, UserRole.SUPER_ADMIN),
+  auth(UserRole.ADMIN, UserRole.SUPER_ADMIN),
+  fileUploader.upload.single("file"),
   userController.createAdmin
 );
 
